4c5e1b2665204e5bc79ab6d19eb8540b84434546
Mike <omvmike@gmail.com>
2013-08-11 23:01:23 +0300

    fix

--- src/training_parser.js
+++ src/training_parser.js
@@ -1,6 +1,6 @@
 var fs = require("fs")
 
-var store_templates = {templates: [], size: 0};
+var store_templates = {templates: [], size: 0, use_fold: undefined};
 
 function most_wanted(size,templates,threshold_percentage){
     var total_count = 0;
@@ -85,6 +85,25 @@ function get_lisp_format(arr, use_fold){
                 }
             }
             while(count > 0)
+        if(use_fold == "fold")
+            do {
+                count = 0;
+                for(var i = (res_arr.length -3); i >= 0; i--){
+                    if((res_arr[i] == "fold") &&
+                        ((res_arr[i+1] == 'c') || (typeof res_arr[i+1] == "object")) &&
+                        ((res_arr[i+2] == 'c') || (typeof res_arr[i+2] == "object")) &&
+                        ((res_arr[i+3] == 'c') || (typeof res_arr[i+3] == "object"))
+                        ){
+                        var part_arr = ['fold', res_arr[i+1], res_arr[i+2], ['lambda', ['x2', 'x3'], res_arr[i+3]]];;
+                        console.log(part_arr);
+                        res_arr[i] = part_arr;
+                        res_arr.splice(i+1,3);
+                        count++;
+                        total_count++;
+                    }
+                }
+            }
+            while(count > 0)
         //var start_arr = ['lambda', 'x'];
     }
     while(total_count)
@@ -103,25 +122,26 @@ function get_templates(size,templates,use_fold){
         suffix = '_tf_';
     }
     var filename = '../train/data'+suffix+size +'.txt';
+    if(fs.existsSync(filename)) {
+        var data = fs.readFileSync(filename);
 
-    var data = fs.readFileSync(filename);
+        console.log(data.toString());
+        var file_text = data.toString();
+        var file_arr=file_text.split("\n");
+        var s='';
+        while((str = file_arr.pop()) != undefined){
+            var reg = /\d/;
 
-    console.log(data.toString());
-    var file_text = data.toString();
-    var file_arr=file_text.split("\n");
-    var s='';
-    while((str = file_arr.pop()) != undefined){
-        var reg = /\d/;
-
-        if(reg.test(str[0])){
-            var sub_str = str.split(" --- ");
-            var temp_arr = sub_str[1].split(",");
-            var template = {count: parseInt(sub_str[0]), template_str: sub_str[1], template_arr: temp_arr, lisp_arr: get_lisp_format(temp_arr, use_fold) };
-            templates.push(template);
-            //console.log(template.count);
-            console.log(template.template_str);
-            //console.log(template.template_arr);
-            console.log(template.lisp_arr);
+            if(reg.test(str[0])){
+                var sub_str = str.split(" --- ");
+                var temp_arr = sub_str[1].split(",");
+                var template = {count: parseInt(sub_str[0]), template_str: sub_str[1], template_arr: temp_arr, lisp_arr: get_lisp_format(temp_arr, use_fold) };
+                templates.push(template);
+                //console.log(template.count);
+                console.log(template.template_str);
+                //console.log(template.template_arr);
+                console.log(template.lisp_arr);
+            }
         }
     }
     return templates;
@@ -131,13 +151,13 @@ function get_templates(size,templates,use_fold){
 
 function get_next_template(size, use_fold){
 
-    if(store_templates.size != size){
+    if((store_templates.size != size) || (store_templates.use_fold != use_fold)){
         var templates = [];
         templates = get_templates(size,[], use_fold);
-        console.log('i am here');
 
         store_templates.size = size;
         store_templates.templates = [];
+        store_templates.use_fold = use_fold;
         for(var i = 0; i < templates.length; i++){
             store_templates.templates[i]=templates[i].lisp_arr;
             //console.log(templates[i].template_arr);
